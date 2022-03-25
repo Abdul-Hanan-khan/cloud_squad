df0352b0a25f10073ca3ac258f9b54e7553428e1
Abdul <abdulhanan.softech@gmail.com>
2022-03-25 17:39:20 +0500

    Initial commit

---
+++ lib/src/utils/settings.dart
@@ -0,0 +1,3 @@
+/// Define App ID and Token
+const APP_ID = 'd8f0632f6b054cbfa2bae67da5fb13e8';
+const Token = '006d8f0632f6b054cbfa2bae67da5fb13e8IABDMkrY5NzyaziUMr0wliNqgeOv98VcwHwtRgWO5Y+DCQx+f9gAAAAAEAAJmhJvJ7o+YgEAAQAouj5i';
